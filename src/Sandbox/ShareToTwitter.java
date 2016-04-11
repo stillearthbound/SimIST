dc85c117f2dac075f0e07ec4fafbaebd3bcd7470
laurenritter <laurenritter@client-104-39-96-85.mobility.psu.edu>
2016-04-11 15:08:54 -0400

    Added to ShareToTwitter

--- src/Sandbox/ShareToTwitter.java
+++ src/Sandbox/ShareToTwitter.java
@@ -5,19 +5,52 @@
  */
 package Sandbox;
 
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 import javax.swing.*;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
  * @author laurenritter
  */
-public class ShareToTwitter extends JFrame{
-    private JButton share;
-    private JTextField enterYoTweet;
-    private JLabel elTitulo;
+public class ShareToTwitter extends JFrame implements ActionListener{
+    private JButton shareButton;
+    private JTextField writeTweet;
+    private JLabel title;
     private JPanel sharePanel;
     
     ShareToTwitter(){
+        initCustomComponents();
+    }
+    
+    public void initCustomComponents(){
+        shareButton = new JButton("Share");
+        writeTweet = new JTextField();
+        title = new JLabel("Write Post");
+        sharePanel = new JPanel();
+        
+        shareButton.addActionListener(this);
+        
+        sharePanel.add(title);
+        sharePanel.add(writeTweet);
+        sharePanel.add(shareButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        shareButtonActionPerformed(e);
+    }
+    
+    public void shareButtonActionPerformed(ActionEvent e){
+        KeyReader keyreader = new KeyReader();
+        
         
     }
 }
