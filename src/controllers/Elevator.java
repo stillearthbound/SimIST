eb7ac3eda4fe1e59bcaf0f92f11390c4b184c1f0
antonizoon <sagnessagiel@gmail.com>
2016-04-13 15:37:49 -0400

    Only People can enter an elevator, though they can carry Units with them

--- src/controllers/Elevator.java
+++ src/controllers/Elevator.java
@@ -8,7 +8,7 @@ import java.util.TreeMap;
 public class Elevator extends Mover {
     protected int direction = 0; // -1 for down, 0 for no direction, 1 for up.
     protected Floor currentFloor;
-    ArrayList<Unit> passengers = new ArrayList<Unit>();
+    ArrayList<Person> passengers = new ArrayList<Person>();
     // which floors have the buttons activated
     
     public Elevator(Building building) {
@@ -41,7 +41,7 @@ public class Elevator extends Mover {
     */
     
     // move to the top floor
-    public void moveUp(Unit passenger){
+    public void moveUp(Person passenger){
         // check if the bottom floor actually contains the passenger
         if(bottom.getUnitList().contains(passenger)) {
             // move passenger to another floor
@@ -51,7 +51,7 @@ public class Elevator extends Mover {
     }
     
     // move to the bottom floor
-    public void moveDown(Unit passenger){
+    public void moveDown(Person passenger){
         // check if the top floor actually contains the passenger
         if(top.getUnitList().contains(passenger)) {
             // move passenger to another floor
--- src/controllers/Person.java
+++ src/controllers/Person.java
@@ -1,4 +1,5 @@
 package controllers;
+import java.util.ArrayList;
 
 /**
  * Interim Person class, representing a mobile person, which is also a Unit.
@@ -6,10 +7,21 @@ package controllers;
  */
 public class Person extends Unit {
     protected Floor destFloor;
+    protected ArrayList<Unit> baggage; // what is the person carrying? Can carry other Units, or even person.
     
     public Person(String name, Floor floor) {
         super(name, floor);
         this.destFloor = floor; // the person is already where they want to be
+    }
+    
+    // return an ArrayList of baggage that the Person is carrying.
+    public ArrayList<Unit> getBaggage() {
+        return this.baggage;
+    }
+    
+    // set an ArrayList of baggage that the Person is carrying.
+    public void setUnitList(ArrayList<Unit> unitList) {
+        this.baggage = baggage;
     }
     
     // find where the passenger wants to go.
