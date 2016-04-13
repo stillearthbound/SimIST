0c530aa42b30aaf3f7446fc5cad8b5b73a5e1aff
lap5486 <lap5486@208-NWD08.up.ist.local>
2016-04-13 15:29:09 -0400

    added booleans for weather, implementing a way to display weather through images.

--- src/sandbox/Weather.java
+++ src/sandbox/Weather.java
@@ -18,25 +18,44 @@ public class Weather {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        String[] weatherCodes = {"tornado","tropical storm","hurricane","severe thunderstorms","thunderstorms","mixed rain and snow","mixed rain and sleet"
+        String[] weatherCodeString = {"tornado","tropical storm","hurricane","severe thunderstorms","thunderstorms","mixed rain and snow","mixed rain and sleet"
                 ,"mixed snow and sleet","freezing drizzle","drizzle","freezing rain","showers","showers","snow flurries","light snow showers","blowing snow","snow","hail","sleet"
-                        ,"dust","foggy","haze","smoky","blustery","windy","cold","cloudy","mostly cloudy (night)","mostly cloudy (day)","partly cloudy (night)","partly cloudy (day)"
-                        ,"clear (night)","sunny","fair (night)","fair (day)","mixed rain and hail","hot","isolated thunderstorms","scattered thunderstorms","scattered thunderstorms"
+                        ,"dust","foggy","haze","smoky","blustery","windy","cold","cloudy","mostly cloudy","mostly cloudy","partly cloudy","partly cloudy"
+                        ,"clear","sunny","fair","fair","mixed rain and hail","hot","isolated thunderstorms","scattered thunderstorms","scattered thunderstorms"
                                 ,"scattered showers","heavy snow","scattered snow showers","heavy snow","partly cloudy","thundershowers","snow showers","isolated thundershowers"};
         
+        boolean cloudy = false;
+        boolean rain = false;
+        boolean snow = false;
+        boolean sun =false;
         
+                
         
         URL weather = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22statecollege%2C%20pa%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
         URLConnection yc = weather.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
                                     yc.getInputStream()));
         String inputLine=in.readLine();
+        int weatherCode = Integer.parseInt(inputLine.substring(119,121));    
+        int temp = Integer.parseInt(inputLine.substring(170,172));
         
-
-         //   System.out.println(inputLine);
-        int code = Integer.parseInt(inputLine.substring(119,121));    
-        System.out.println(weatherCodes[(code + 1)]);  
-        System.out.println(inputLine.substring(170,172) + "°F");  
+       // System.out.println(inputLine);
+        if ((weatherCode > 25 && weatherCode < 31 )|| weatherCode == 44){
+            cloudy = true;
+        }
+        else if(weatherCode < 5 ||(weatherCode >7 && weatherCode < 13 )|| weatherCode == 17 || weatherCode ==35 || (weatherCode >36 && weatherCode > 41) || weatherCode == 45 || weatherCode == 47){
+            rain = true;
+        }
+        else if(weatherCode > 4 && weatherCode < 8 || weatherCode >12 && weatherCode < 17 || weatherCode >40 && weatherCode < 44 || weatherCode == 46){
+            snow = true;
+        }
+        else if(weatherCode > 22 && weatherCode < 26 || weatherCode > 30 && weatherCode < 35|| weatherCode == 36){
+            sun = true;
+        }
+        
+        
+        System.out.println(weatherCodeString[(weatherCode)]);  
+        System.out.println(temp + "°F");  
         
         in.close();
         
