d2198eb0e1aeade88efbc3fedab504a65b06a669
lap5486 <lap5486@PATTEEW116-56.win.psu.edu>
2016-04-13 21:12:21 -0400

    made  changes to weatherframe

--- src/sandbox/WeatherFrame.java
+++ src/sandbox/WeatherFrame.java
@@ -5,16 +5,25 @@ import javax.swing.*;
  * @author Matthew
  */
 public class WeatherFrame extends JFrame{
-    private ImageIcon sunnyImage;
-    private ImageIcon cloudyImage;
-    private ImageIcon rainyImage;
-    private ImageIcon snowyImage;
+    ImageIcon sunnyImage;
+    ImageIcon cloudyImage;
+    ImageIcon rainyImage;
+    ImageIcon snowyImage;
+    JLabel tempLable;
+    JButton weatherIcon;
     WeatherFrame(){
         JFrame f = new JFrame();
+        JPanel p = new JPanel();
+        tempLable = new JLabel();
+        weatherIcon = new JButton();
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(150,150);
         f.setResizable(false);
+        f.add(p);
+        p.add(tempLable);
+        p.add(weatherIcon);
+        
         sunnyImage = new ImageIcon("src/sandbox/WeatherImages/Sunny.gif");
         cloudyImage = new ImageIcon("src/sandbox/WeatherImages/Cloudy.gif");
         rainyImage = new ImageIcon("src/sandbox/WeatherImages/Rainy.gif");
