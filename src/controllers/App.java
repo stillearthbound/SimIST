1b73862ecf3367a4692736f7bb7bf6e8e702e83e
Mecca Shabazz <meccas559@gmail.com>
2016-04-17 15:14:04 -0400

    update

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -7,12 +7,11 @@ import entities.Room;
 public class App {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
+
         //FrameTester test = new FrameTester();
 
         FloorController controller = new FloorController();
 
->>>>>>> Stashed changes
+
     }
 }
--- src/controllers/Floor.java
+++ src/controllers/Floor.java
@@ -1,25 +1,19 @@
 package controllers;
 
-<<<<<<< Updated upstream
+
 import java.util.ArrayList;
-=======
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import views.*;
->>>>>>> Stashed changes
+
 
 public class Floor {
 
     protected ArrayList<Unit> unitList = new ArrayList<Unit>();
-<<<<<<< Updated upstream
+
     protected int level; // numerical level of the floor: negative is underground
     
     public Floor(int level) {
         this.level = level;
     }
-=======
->>>>>>> Stashed changes
+
 
     public void addUnit(Unit u) {
         this.unitList.add(u);
@@ -36,7 +30,7 @@ public class Floor {
     public void setUnitList(ArrayList<Unit> unitList) {
         this.unitList = unitList;
     }
-<<<<<<< Updated upstream
+
     
     public int getLevel() {
         return this.level;
@@ -45,8 +39,5 @@ public class Floor {
     public void setLevel(int level) {
         this.level = level;
     }
-=======
 
-
->>>>>>> Stashed changes
 }
